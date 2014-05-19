d7055a6eb100bc92c134e866a6ce406eef325c26
Jonghwa Woo <wjh0630@gmail.com>
2014-05-20 00:49:33 +0900

    Add iterator

--- MovieDatabase/LinkedList.h
+++ MovieDatabase/LinkedList.h
@@ -1,20 +1,59 @@
 #pragma once
 
-template<typename T>
-class LinkedList 
+template<class T, class A = std::allocator<T>>
+class LinkedList final
 {
-public:
-    LinkedList() {}
-
-    unsigned size() { return size_; }
-
-private:
-    unsigned size_;
     struct _Node
     {
-        _Node(T v) : val(v), next(nullptr) {}
+        _Node() {}
+        _Node(const T &v) : val(v), next(nullptr) {}
+        _Node(T &&v) : val(std::move(v)), next(nullptr) {}
 
         T val;
         _Node *next;
     };
+public:
+    typedef A allocator_type;
+    typedef typename A::value_type value_type; 
+    typedef typename A::reference reference;
+    typedef typename A::const_reference const_reference;
+    typedef typename A::difference_type difference_type;
+    typedef typename A::size_type size_type;
+
+    class iterator 
+    {
+    public:
+        typedef typename A::difference_type difference_type;
+        typedef typename A::value_type value_type;
+        typedef typename A::reference reference;
+        typedef typename A::pointer pointer;
+        typedef std::forward_iterator_tag iterator_category;
+
+        iterator(_Node *x)
+        {
+
+        }
+        iterator(const iterator&);
+        ~iterator();
+
+        reference operator*() { return node->val; }
+        pointer operator->() { return &node->val; }
+
+    private:
+        _Node *node;
+    };
+
+    LinkedList() : head(&end_), size_(0) {}
+    ~LinkedList() {}
+
+    iterator begin() { return iterator(head); }
+    iterator end() { return iterator(&end_); }
+
+    unsigned size() { return size_; }
+
+    _Node end_;
+    _Node *head;
+
+    unsigned size_;
 };
+
--- MovieDatabase/Main.cpp
+++ MovieDatabase/Main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include <string>
 #include "LinkedList.h"
+#include <list>
+void test()
+{
+    std::list<int> lst;
+    lst.insert(lst.end(), 1);
+    lst.insert(lst.end(), 2);
+    lst.insert(lst.begin(), 3);
+
+    for(auto x : lst)
+        std::cout << x << std::endl;
+}
 
 void process(std::string &line)
 {
@@ -8,6 +19,8 @@ void process(std::string &line)
 
 int main()
 {
+    test();
+
     for(std::string line; std::getline(std::cin, line); )
     {
         if(line == "quit")
